c0638c6dc851f2a7d7f5170079ae94e28e6039ae
Jérémie Gavrel <gavrel.jeremie@gmail.com>
2021-12-09 22:18:27 +0100

    Implement plan_n_tracks for n <= len(track_list)

--- concert_planning.py
+++ concert_planning.py
@@ -17,18 +17,35 @@ def find_value(sorted_list, value, delta=0):
     return False
 
 
-def find_sum_of_2_values(sorted_list, value, lo, hi, delta):
-    """Check if sorted_list contains two elements whose sum is the required
-    value (± delta if specified)"""
+def find_sum_of_2_values(sorted_list, target_value, lo, hi, delta):
+    """Check if sorted_list contains two elements between indices lo and hi
+    whose sum is the target value (± delta if specified)"""
 
     while lo < hi:
-        total_length = sorted_list[lo] + sorted_list[hi]
-        if abs(total_length - value) <= delta:
+        total_value = sorted_list[lo] + sorted_list[hi]
+        if abs(total_value - target_value) <= delta:
             return True
-        elif total_length < value:
+        elif total_value < target_value:
             lo += 1
         else:
             hi -= 1
+
+    return False
+
+
+def find_sum_of_n_values(sorted_list, target_value, n, lo, hi, delta):
+    """Check if sorted_list contains n elements between indices lo and hi
+    whose sum is the target value (± delta if specified)"""
+
+    if n == 2:
+        return find_sum_of_2_values(sorted_list, target_value, lo, hi, delta)
+
+    for i, i_value in enumerate(sorted_list[lo:], lo):
+        new_target_value = target_value - i_value
+        if find_sum_of_n_values(
+            sorted_list, new_target_value, n - 1, i + 1, hi, delta
+        ):
+            return True
 
     return False
 
@@ -86,10 +103,10 @@ def plan_n_tracks(track_list, concert_premiere_length, n=3, delta=0):
         return find_sum_of_2_values(
             sorted_list, concert_premiere_length, 0, nb_tracks - 1, delta
         )
-    elif n == 3:
-        return plan_3_tracks(track_list, concert_premiere_length, delta)
     else:
-        raise ValueError("TODO n > 3")
+        return find_sum_of_n_values(
+            sorted_list, concert_premiere_length, n, 0, nb_tracks - 1, delta
+        )
 
 
 def test():
@@ -112,6 +129,11 @@ def test():
     assert plan_3_tracks(track_list, 74, delta=5)
     assert not plan_3_tracks(track_list, 75, delta=5)
 
+    # Test the sanity checks for plan_n_tracks
+    assert not plan_n_tracks(track_list, 88, n=8)
+    assert not plan_3_tracks(track_list, 8)
+    assert not plan_3_tracks(track_list, 300)
+
     # Test plan_n_tracks with n=1
     assert plan_n_tracks(track_list, 14, n=1)
     assert not plan_n_tracks(track_list, 10, n=1)
@@ -123,6 +145,21 @@ def test():
     assert not plan_n_tracks(track_list, 15, n=2)
     assert plan_n_tracks(track_list, 34, n=2, delta=5)
     assert not plan_n_tracks(track_list, 35, n=2, delta=5)
+
+    # Test plan_n_tracks with n=3 (repeat the first tests with the new method)
+    assert plan_n_tracks(track_list, 22, n=3)
+    assert plan_n_tracks(track_list, 13, n=3)
+    assert not plan_n_tracks(track_list, 43, n=3)
+    assert not plan_n_tracks(track_list, 12, n=3)
+    assert plan_n_tracks(track_list, 66, n=3, delta=5)
+    assert plan_n_tracks(track_list, 74, n=3, delta=5)
+    assert not plan_n_tracks(track_list, 75, n=3, delta=5)
+
+    # Test plan_n_tracks with n=6
+    assert plan_n_tracks(track_list, 48, n=6)
+    assert not plan_n_tracks(track_list, 68, n=6)
+    assert plan_n_tracks(track_list, 68, n=6, delta=5)
+    assert not plan_n_tracks(track_list, 67, n=6, delta=5)
 
     print("All tests executed successfully.")
 
